a3bced26834494c1ab0f1360c64d6839f2124c43
Kristin Barr <kristinbarr@gmail.com>
2020-06-29 18:53:11 -0400

    completed routes working

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -2,7 +2,9 @@ const db = require('../data/db-config')
 
 module.exports = {
   getRecipes,
+  getRecipesThatUseIngredient,
   getShoppingList,
+  deleteRecipe,
   getInstructions,
 }
 
@@ -14,7 +16,21 @@ function getRecipes() {
   return db('recipes')
 }
 
-// SELECT name, quantity
+// SELECT r.name
+// FROM recipes as r
+// JOIN recipe_ingredients as ri
+// ON r.id = ri.recipe_id
+// WHERE ri.ingredient_id = 2;
+
+// all recipes in the system that utilize a single ingredient
+function getRecipesThatUseIngredient(ingredient_id) {
+  return db('recipes as r')
+    .join('recipe_ingredients as ri', 'r.id', 'ri.recipe_id')
+    .select('r.name')
+    .where('ri.ingredient_id', ingredient_id)
+}
+
+// SELECT i.name, ri.quantity, ri.unit
 // FROM recipe_ingredients as ri
 // JOIN ingredients as i
 // ON ri.ingredient_id = i.id
@@ -22,16 +38,28 @@ function getRecipes() {
 
 // getShoppingList(recipe_id): should return a list of all ingredients and quantities for a given recipe
 function getShoppingList(id) {
-  return db('recipe_ingredients as r')
-    .join('ingredients as i', 'r.ingredient_id', 'i.id')
-    .select('i.name', 'r.quanity', 'r.unit')
-    .where({ 'r.recipe_id': id })
+  return db('recipe_ingredients as ri')
+    .join('ingredients as i', 'ri.ingredient_id', 'i.id')
+    .select('i.name', 'ri.quantity', 'ri.unit')
+    .where({ 'ri.recipe_id': id })
+}
+
+// DELETE 
+// FROM recipes
+// WHERE id = 1;
+
+// delete a recipe by recipe id
+function deleteRecipe(id) {
+  return db('recipes')
+  .where({ id: req.params.id })
+  .del()
 }
 
 // SELECT s.step_number, s.description
 // FROM recipes as r
 // JOIN steps as s
-// ON r.id = s.recipe_id;
+// ON r.id = s.recipe_id
+// WHERE r.id = 1;
 
 // getInstructions(recipe_id): should return a list of step by step instructions for preparing a recipe
 function getInstructions(id) {
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,40 +1,54 @@
 const express = require('express')
 const db = require('../data/db-config.js')
-const Recipes = require('./recipes-model')
+const recipes = require('./recipes-model')
 const router = express.Router()
 
-
-// GET /api/recipes/: all recipes (without details about ingredients or steps)
+// GET /api/recipes - returns all recipes (without details about ingredients or steps)
 router.get('/recipes', (req, res) => {
-  db('recipes')
-  .then(recipes => {
-    res.status(200).json(recipes)
-  })
-  .catch(error => {
-    res.status(500).json(error)
-  })
+  recipes.getRecipes()
+    .then(recipes => {
+      res.status(200).json(recipes)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 })
 
-// GET /api/recipes/:id/shoppingList: a list of ingredients and quantites for a single recipe
-router.get("/recipes/:id/shoppingList", async (req, res) => {
-  console.log(re)
-  try {
-    const ingredients = await Recipes.getShoppingList(req.params.id);
-    res.status(200).json(ingredients);
-  } catch (error) {
-    res.status(500).json({ error: "server error :(" });
-  }
-});
+// GET /api/recipes/:id/shoppingList - returns a list of ingredients and quantites for a single recipe
+router.get('/recipes/:id/shoppingList', (req, res) => {
+  const { id } = req.params
+  recipes
+  .getShoppingList(id)
+  .then(list => {
+      console.log('steps:', list)
+      res.status(200).json(list)
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'server error :(' })
+    })
+})
 
-// GET /api/recipes/:id/instructions: a correctly ordered list of how to prepare a single recipe
-// GET /api/ingredients/:id/recipes: all recipes in the system that utilize a single ingredient
+// GET /api/recipes/:id/instructions - returns a correctly ordered list of how to prepare a single recipe
+router.get('/recipes/:id/instructions', (req, res) => {
+  const { id } = req.params
+  recipes
+    .getInstructions(id)
+    .then(steps => {
+      if (steps.length) {
+        res.status(200).json(steps)
+      } else {
+        res.status(404).json({ message: 'Could not find steps for given recipe' })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get steps for given recipe' })
+    })
+})
 
-
-// remove recipe
+// DELETE /api/recipes/:id - remove recipe
 router.delete('/recipes/:id', (req, res) => {
-  db('recipes')
-    .where({ id: req.params.id })
-    .del()
+  const { id } = req.params
+  recipes.deleteRecipe(id)
     .then(count => {
       if (count > 0) {
         res.status(204).end()
@@ -47,24 +61,32 @@ router.delete('/recipes/:id', (req, res) => {
     })
 })
 
-// get all ingredients from the database
-router.get('/ingredients', (req, res) => {
-  db('ingredients')
-    .then(ingredients => {
-      res.status(200).json(ingredients)
+// ***** INGREDIENTS *****
+
+// GET /api/ingredients/:id/recipes - all recipes in the system that utilize a single ingredient
+router.get('/ingredients/:id/recipes', (req, res) => {
+  const { id } = req.params
+  recipes.getRecipesThatUseIngredient(id)
+  .then(recipes => {
+    console.log('recipes:', recipes)
+      if (recipes.length < 0) {
+        res.status(404).json({ message: 'No recipes found' })
+      } else {
+        res.status(200).json(recipes)
+      }
     })
     .catch(error => {
-      res.status(500).json(error)
+      console.log('error:', error)
+      res.status(500).json({ message: 'Could not complete the request for recipes'})
     })
 })
 
-// add ingredient
+// POST /api/ingredients - add an ingredient
 router.post('/ingredients', (req, res) => {
   db('ingredients')
     .insert(req.body)
     .then(ids => {
       const id = ids[0]
-
       db('ingredients')
         .where({ id })
         .first()
